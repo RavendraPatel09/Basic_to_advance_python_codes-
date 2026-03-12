29bec54fa20ba24ae6868b9f5d7e6848f84b14ce
patelsamerth9 <patelsamerth9@gmail.com>
2026-03-12 22:00:06 +0530

    Linear search in array element

---
+++ Linear Search.py
@@ -0,0 +1,10 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+numbers = [10, 23, 45, 70, 11, 15]
+target_value = 70
+result = linear_search(numbers, target_value)
+print(result)
