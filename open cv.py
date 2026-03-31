2f7670418ac7fe46412149ac3fbc87823b9549b1
patelsamerth9 <patelsamerth9@gmail.com>
2026-03-31 14:19:00 +0530

    some basic concepts of git commit message

--- open cv.py
+++ open cv.py
@@ -1,12 +1,11 @@
 import cv2
-<<<<<<< HEAD
 import cv2.data
 import os
 
 from numpy import eye
 classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')  
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 while True:
     status,frame = cap.read()
     if not status:
@@ -29,16 +28,3 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
-
-=======
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    cv2.imshow('Live Webcam Feed', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
->>>>>>> 775b7034bdbbdf99f32c735c6a2ca588cb54215f
