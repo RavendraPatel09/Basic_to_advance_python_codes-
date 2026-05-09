367d8ab04a9625757bec6c099b634f28f900a8ed
patelsamerth9 <patelsamerth9@gmail.com>
2026-05-10 00:06:27 +0530

    Add new Python scripts for generating discounted bills, calculating grades, guessing numbers, determining heart rates, and applying image filters for enhanced functionality and demonstration of concepts

--- GenerateDiscountedBill.py
+++ GenerateDiscountedBill.py
@@ -1,12 +1,10 @@
 price = 500
 quantity = 3
 total = price * quantity
-
 if total > 1000:
     discount = total * 0.1
 else:
     discount = total * 0.05
-
 final = total - discount
 print("Total:", total)
 print("Discount:", discount)
